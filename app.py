e72a3ca4fe5d803b5a6f2058c958e0865493c7ff
Ge-raldh25 <nzabamounamou@gmail.com>
2025-07-17 11:28:35 +0100

    update app python

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def devops_info():
-try:
+	try:
 # On récupère le résumé de l'article Wikipedia "DevOps"
-response = requests.get("https://en.wikipedia.org/api/rest_v1/page/summary/DevOps")
-data = response.json()
-summary = data.get("extract", "Aucune information trouvée.")
-except Exception as e:
-summary = f"Erreur lors de la récupération des informations : {e}"
+	    response = requests.get("https://en.wikipedia.org/api/rest_v1/page/summary/DevOps")
+	    data = response.json()
+            summary = data.get("extract", "Aucune information trouvée.")
+	except Exception as e:
+	    summary = f"Erreur lors de la récupération des informations : {e}"
 
-html = f"""
-<h1 style='color: green;'> Informations sur le DevOps</h1>
-<p>{summary}</p>
-"""
-return html
+	    html = f"""
+	            <h1 style='color: green;'> Informations sur le DevOps</h1>
+	            <p>{summary}</p>
+                    """
+	return html
 
-if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000)
+	if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000)
